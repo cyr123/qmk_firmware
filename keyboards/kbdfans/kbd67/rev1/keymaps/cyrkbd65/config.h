98e4746aa5d489e74ac4160a1334000ee83440c9
Erik Gustavsson <erik.gustavsson@saabgroup.com>
2020-01-22 13:34:27 +0100

    Remove backlight functions and retro tapping

--- keyboards/kbdfans/kbd67/rev1/keymaps/cyrkbd65/config.h
+++ keyboards/kbdfans/kbd67/rev1/keymaps/cyrkbd65/config.h
@@ -1,10 +1,4 @@
 #pragma once
 
-/* send tap key if no layer key was used even after tap delay */
-#define TAPPING_TERM 50
-#define RETRO_TAPPING
-
-/* turn off RGB when computer sleeps */
-#ifdef RGB_DI_PIN
-#define RGBLIGHT_SLEEP
-#endif
+#define TAPPING_TERM 250
+#define PERMISSIVE_HOLD
